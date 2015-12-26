c00a71933b7fb6725fc21f718f66a06bf82c33c8
Greg <Greg>
2015-12-27 02:17:31 +0300

    Made media for main page

--- js/bnovo_form.js
+++ js/bnovo_form.js
@@ -4,23 +4,22 @@ function showBnovo() {
 	btn = $('.button')[0];
 	bnovo.style.cssText="top: 20px;"
 	btn.style.cssText="display: none;"
-	// $('.menu')[0].style.cssText="margin-top: 24.1%;";
 }
 
 // Bnovo form
 Bnovo_Widget.init(function(){
-  Bnovo_Widget.open('_bn_widget_', {
-	  type: "horizontal",
-	  lcode: "1234567890",
-	  lang: "ru",
-	  width: "960",
-	  background: "#2D2D2D",
-	  bg_alpha: "100",
-	  padding: "88",
-	  font_size: "12",
-	  btn_background: "#719E5B",
-	  btn_background_over: "#719E5B",
-	  btn_textcolor: "#FFFFFF",
-	  btn_textover: "#FFFFFF"
+   Bnovo_Widget.open('_bn_widget_', {
+       type: "vertical",
+       lcode: "1234567890",
+       lang: "ru",
+       width: "80%",
+       background: "#555555",
+       bg_alpha: "75",
+       padding: "18",
+       font_size: "12",
+       btn_background: "#719E5B",
+       btn_background_over: "#719E5B",
+       btn_textcolor: "#FFFFFF",
+       btn_textover: "#FFFFFF"
   });
 });
