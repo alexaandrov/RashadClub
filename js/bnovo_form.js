b01d4b4cc8ed0a7cec8964357cd829eba89e1449
Greg <Greg>
2015-12-27 00:39:15 +0300

    Added bnovo form to main page, and made the button, which show it.

---
+++ js/bnovo_form.js
@@ -0,0 +1,26 @@
+// Show hiden Bnovo form, and hide the button
+function showBnovo() {
+	bnovo = document.getElementById('bn_wrapper');
+	btn = $('.button')[0];
+	bnovo.style.cssText="top: 20px;"
+	btn.style.cssText="display: none;"
+	$('.menu')[0].style.cssText="margin-top: 275px;";
+}
+
+// Bnovo form
+Bnovo_Widget.init(function(){
+  Bnovo_Widget.open('_bn_widget_', {
+	  type: "horizontal",
+	  lcode: "1234567890",
+	  lang: "ru",
+	  width: "960",
+	  background: "#2D2D2D",
+	  bg_alpha: "100",
+	  padding: "88",
+	  font_size: "12",
+	  btn_background: "#719E5B",
+	  btn_background_over: "#719E5B",
+	  btn_textcolor: "#FFFFFF",
+	  btn_textover: "#FFFFFF"
+  });
+});
