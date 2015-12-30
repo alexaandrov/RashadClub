35e355995ade3739a82ed8f605ea9b2ff5033e37
Greg <Greg>
2015-12-30 10:53:44 +0000

    added additional menu to all hotels in graph apartments, made telephones in top as absolute block.

---
+++ js/header_additional_menu.js
@@ -0,0 +1,29 @@
+	function showUl() {
+		var li = document.getElementById('li_apart');
+		var ul = document.getElementById('ul_apart');
+		ul.style.cssText="display:block";
+	}
+
+	function hideUl() {
+		var li = document.getElementById('li_apart');
+		var ul = document.getElementById('ul_apart');
+		ul.style.cssText="dispay:none";
+	}
+
+// OBJECTIVE STYLE, error
+
+// var headerAdditionalMenu = {
+// 	items:{li = document.getElementById('li_apart'),
+// 	ul = document.getElementById('ul_apart'),],
+
+// 	showUl: function() {
+// 		ul.style.cssText="display:block";
+// 	},
+// }
+
+// window.onload = function() {
+// 	headerAdditionalMenu.showUl();
+// }
+
+
+
