d9b03a921b5bc0df57bda2d7f76c68e0b19001aa
alexaandrov <alexaandrov@gmail.com>
2016-01-21 12:31:58 +0300

    added ajax querry to all emika forms

--- js/emika.js
+++ js/emika.js
@@ -1,33 +1,48 @@
 $(function () {
-	var btn = $(".emika:eq(1) .submit");
-		btn.click(function(e) {
-		e.preventDefault();
-		var date = $(".emika:eq(1) :input").serializeArray();
+	var emikaBtnCheck = $(".emika:eq(0) .submit");
+	var emikaMobBtnCheck = $(".emika:eq(1) .submit");
+
+	function sendInfo(date) {
 		var checkIn = date[0].value;
 		var checkOut = date[1].value;
 		var adults = date[2].value;
 		var children = date[3].value;
 		
-			function funcBefore() {
-				$("#txt").text ("Ожидание данных");
-			}
+		function funcBefore() {
+			$("#txt").text ("Ожидание данных");
+		}
 			
-			function funcSucces(data) {
-				$("#txt").text (data);
-			}
+		function funcSucces(data) {
+			alert("Данные успешно отправлены");
+			$("#txt").text (data);
+		}
 			
-			$.ajax({
-				url: "../php/emika.php",
-				type: "POST",
-				data: {
-				  checkIn: '2016-01-02',
-				  checkOut: '2016-02-01',
-				  adults: 5,
-				  children: 3
-				},
-				beforeSend: funcBefore,
-				success: funcSucces
-				
-			});
+		$.ajax({
+			url: "../php/emika.php",
+			type: "POST",
+			data: {
+			  checkIn: checkIn,
+			  checkOut: checkOut,
+			  adults: adults,
+			  children: children
+			},
+			beforeSend: funcBefore,
+			success: funcSucces
+		});
+	}
+
+	// Если нажата кнопка "Проверить" компьютере
+	emikaBtnCheck.click(function(e) {
+		e.preventDefault();
+		var date = $(".emika:eq(0) :input").serializeArray();
+		sendInfo(date);
+	});
+		
+	// Если нажата кнопка "Проверить" с телефона
+	emikaMobBtnCheck.click(function(e) {
+		e.preventDefault();
+		var date = $(".emika:eq(1) :input").serializeArray();
+		sendInfo(date);
 	});
 });
+
